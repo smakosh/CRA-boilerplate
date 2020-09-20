1988183670e5961c542530d56aecbc2fdd38053a
Ismail Ghallou <ismai23l@hotmail.com>
2020-09-20 12:48:48 +0100

    Update index.js

--- src/config/index.js
+++ src/config/index.js
@@ -1,4 +1,4 @@
-export default {
+module.exports = {
   BASE_URL: process.env.REACT_APP_PROD_API,
   UPLOAD_SERVICE_API: process.env.REACT_APP_UPLOAD_SERVICE_API,
   EMAIL_API: process.env.REACT_APP_EMAIL_API,
