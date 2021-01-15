4d65970489a161c0feb0f3d7dc2805819f35af16
smakosh <ismai23l@hotmail.com>
2021-01-14 21:29:36 -0800

    chore: improved typing on the user provider

--- src/features/auth/interfaces/index.ts
+++ src/features/auth/interfaces/index.ts
@@ -3,9 +3,11 @@ export type UserActions =
   | { type: 'LOGOUT' }
   | { type: 'LOADING_TRUE' }
   | { type: 'LOADING_FALSE' }
+
 export type StateUser = {
   isLoggedIn: boolean
   loading: boolean
   data: any // Type it as your returned user data from your API
 }
+
 export type DispatchUser = (action: UserActions) => void
